64c1d155ea882e81502a27153b4b6995e4f122ff
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-09 14:22:00 -0500

    finsihed 4th one

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -15,17 +15,64 @@ module.exports = {
   },
 
   list(req, res) {
-      Contacts.all()
-        .then(contacts => res.status(200).send(contact))
+      Contacts.findAll()
+        .then(contacts => res.status(201).send(contacts))
         .catch(error => res.status(400).send(error));
     },
 
     onecontact(req, res) {
-        Contacts.findByid()(req.params.id)
+        Contacts.findById(req.params.id)
           .then(contacts => res.status(200).send(contacts))
           .catch(error => res.status(400).send(error));
       },
       // My id is 4, keep gettting back {}.
 
 
+
+updateStuff (req, res){
+  Contacts.update(req.body, {
+    fields: ['name', 'phonenumber', 'city', 'state', 'photo_url'],
+      where: {
+          id: req.params.id
+        }
+  })
+  .then((contacts) => res.status(200).send(contacts))
+  .catch((error) => res.status(400).send(error));
+}
+
+
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// update(req, res) {
+//  return Contacts
+// //  where: {
+// //    authorId: 4
+// //  }
+//    .findById(req.params.id)
+//    .then(contacts => {
+//      return contacts
+//        .update({
+//          name: req.body.name,
+//          phonenumber: req.body.phonenumber,
+//          city: req.body.city,
+//          state: req.body.state,
+//          photo_url: req.body.photo_url,
+//        })
+//        .then(() => res.status(200).send(contacts))
+//        .catch((error) => res.status(400).send(error));
+//    })
+// },
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,4 +4,6 @@ const ContactsController= require("../controllers/contacts");
 module.exports = (app) => {
    app.post('/contacts', ContactsController.create);
     app.get('/contacts', ContactsController.list);
+    app.get('/contacts/:id', ContactsController.onecontact);
+    app.put('/contacts/:id', ContactsController.updateStuff);
 };
