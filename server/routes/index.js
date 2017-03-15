fd200a1a92b8ff5d446cbb8c8b495c43e48edba9
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-15 17:09:43 -0400

    added routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,13 @@
+
 const ContactsController= require("../controllers/contacts");
 
 
 module.exports = (app) => {
+  app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+   });
    app.post('/contacts', ContactsController.create);
     app.get('/contacts', ContactsController.list);
     app.get('/contacts/:id', ContactsController.onecontact);
